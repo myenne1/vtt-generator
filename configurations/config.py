19a2f0ce468d04c1272ce7d2ce5dd09713d14aea
myenne1 <yennesmuhanad@gmail.com>
2025-06-11 14:17:31 -0500

    Implemented basic security features

--- configurations/config.py
+++ configurations/config.py
@@ -8,6 +8,10 @@ class Settings(BaseSettings):
     BUCKET_NAME: str
     TIME_WINDOW: int
     MIME_CHECKING: bool
+    AWS_ACCESS_KEY_ID: str | None = None
+    AWS_SECRET_ACCESS_KEY: str | None = None
+    AWS_REGION: str | None = None
+
     class Config:
         env_file = "./configurations/configs.env"
         env_file_encoding = "utf-8"
